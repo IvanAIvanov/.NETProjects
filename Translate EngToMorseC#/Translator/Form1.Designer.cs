bc1876ef82927c5a899fd0a107e65ca3d8352981
Ivan Ivanov <ivan_a_ivanov@hotmail.com>
2018-06-17 19:04:01 +0300

    TranslateEnglishToMorse

---
+++ Translate EngToMorseC#/Translator/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.howTouseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howTouseToolStripMenuItem,
+            this.aboutMeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(673, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // howTouseToolStripMenuItem
+            // 
+            this.howTouseToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.howTouseToolStripMenuItem.Name = "howTouseToolStripMenuItem";
+            this.howTouseToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.howTouseToolStripMenuItem.Text = "How to &use";
+            this.howTouseToolStripMenuItem.Click += new System.EventHandler(this.howTouseToolStripMenuItem_Click);
+            // 
+            // aboutMeToolStripMenuItem
+            // 
+            this.aboutMeToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.aboutMeToolStripMenuItem.Name = "aboutMeToolStripMenuItem";
+            this.aboutMeToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.aboutMeToolStripMenuItem.Text = "&About me";
+            this.aboutMeToolStripMenuItem.Click += new System.EventHandler(this.aboutMeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your English sentences:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Morse Code translation:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(214, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Convert to &Morse Code";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(470, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 37);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 238);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(383, 65);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(278, 234);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(673, 358);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Translator 1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem howTouseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Translate EngToMorseC#/Translator/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+// Created by Ivan Ivanov
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        private void howTouseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enter a sentence in English or Morse code to get the equivalent translation.");
+        }
+
+        private void aboutMeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Translator Morse Code Ver. 1.0\n Created by Ivan Ivanov\n" + "ivan_a_ivanov@hotmail.com");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<char, string> _codes;
+
+            {
+                _codes = new Dictionary<char, string>();
+                _codes.Add('A', ".-");
+                _codes.Add('a', ".-");
+
+                _codes.Add('B', "-...");
+                _codes.Add('b', "-...");
+
+                _codes.Add('C', "-.-.");
+                _codes.Add('c', "-.-.");
+
+                _codes.Add('D', "-..");
+                _codes.Add('d', "-..");
+
+                _codes.Add('E', ".");
+                _codes.Add('e', ".");
+
+                _codes.Add('F', "..-.");
+                _codes.Add('f', "..-.");
+
+                _codes.Add('G', "--.");
+                _codes.Add('g', "--.");
+
+                _codes.Add('H', "....");
+                _codes.Add('h', "....");
+
+                _codes.Add('I', "..");
+                _codes.Add('i', "..");
+
+                _codes.Add('J', ".---");
+                _codes.Add('j', ".---");
+
+                _codes.Add('K', "-.-");
+                _codes.Add('k', "-.-");
+
+                _codes.Add('L', ".-..");
+                _codes.Add('l', ".-..");
+
+                _codes.Add('M', "--");
+                _codes.Add('m', "--");
+
+                _codes.Add('N', "-.");
+                _codes.Add('n', "-.");
+
+                _codes.Add('O', "---");
+                _codes.Add('o', "---");
+
+                _codes.Add('P', ".--.");
+                _codes.Add('p', ".--.");
+
+
+                _codes.Add('Q', "--.-");
+                _codes.Add('q', "--.-");
+
+
+                _codes.Add('R', ".-.");
+                _codes.Add('r', ".-.");
+
+
+                _codes.Add('S', "...");
+                _codes.Add('s', "...");
+
+                _codes.Add('T', "-");
+                _codes.Add('t', "-");
+
+                _codes.Add('U', "..-");
+                _codes.Add('u', "..-");
+
+                _codes.Add('V', "...-");
+                _codes.Add('v', "...-");
+
+                _codes.Add('W', ".--");
+                _codes.Add('w', ".--");
+
+                _codes.Add('X', "-..-");
+                _codes.Add('x', "-..-");
+
+                _codes.Add('Y', "-.--");
+                _codes.Add('y', "-.--");
+
+                _codes.Add('Z', "--..");
+                _codes.Add('z', "--..");
+
+                _codes.Add('1', ".----");
+                _codes.Add('2', "..---");
+                _codes.Add('3', "...--");
+                _codes.Add('4', "....-");
+                _codes.Add('5', ".....");
+                _codes.Add('6', "-....");
+                _codes.Add('7', "--...");
+                _codes.Add('8', "---..");
+                _codes.Add('9', "----.");
+                _codes.Add('0', "-----");
+            }
+
+
+             while (textBox1.Text.Length > 0)
+             {
+                 char[] c1 = textBox1.Text.ToCharArray(0, 1);
+                 char c2 = c1[0];
+                 if (_codes.ContainsKey(c2)) textBox2.Text += _codes[c2];
+
+                 textBox1.Text = textBox1.Text.Substring(1, textBox1.Text.Length - 1);
+             }
+        
+        }
+    }
+}
